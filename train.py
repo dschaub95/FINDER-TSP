de1810e71a090dc0193f5f4e933672816c421f72
Darius Schaub <schaub.darius@gmail.com>
2021-08-24 19:59:58 +0000

    small refinements

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ def main():
     start_config_path = 'train_configs/train_config.txt'
     final_config_path = 'models/tsp_2d/current_config.txt'
     copy(start_config_path, final_config_path)
-    dqn = FINDER(config_path=final_config_path)
+    dqn = FINDER(train_config_path=final_config_path)
     dqn.Train()
     if str(sys.argv[1]) == 'save':
         save_best_model(dqn, config_path=final_config_path)
